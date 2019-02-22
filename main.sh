ed0131b8f688c1297b1473c777b18b2d43a56768
张宇威 <zhangyuwei@gomo.com>
2019-02-22 21:27:27 +0800

    add git / rubbishcode

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 . ./replace.sh
-. ./package.sh
+. ./rubbishCode.sh
+
 #文件配置
 filepath="/Users/zy/WorkSpace/Test/ShellTest/Co_pay_PayNotificationSDK"
 newFilePath="/Users/zy/WorkSpace/Test/ShellTest/package/TESTPayNotificationSDK"
@@ -30,6 +31,8 @@ function getdir(){
         #替换开始
         replace $file $newfile $oldPrefix $newPrefix $sdkName
         #替换结束
+        #增加垃圾代码
+        rubbishCode $newfile 
     fi
     done
 }
@@ -40,27 +43,28 @@ function main(){
     rm -rf $newFilePath
     echo "删除 路径下文件" $newFilePath
     changePath=${filepath}
-    getdir $changePath 
+    getdir $changePath
+    # 后续步骤暂时关闭
     # 创建git
-    cd $newFilePath
-    git init && git add . && git commit -m "build" 
-    podspec=${sdkName/$oldPrefix/$newPrefix}
-    pod package ${podspec}.podspec —force --spec-sources='https://github.com/CocoaPods/Specs.git,http://gerrit.3g.net.cn/gomo_ios_specs' --no-mangle --gomoad --exclude-deps
-    versionStr=`sed -n "/s.version *=/p" ${podspec}.podspec`
-    versionStr=`echo $versionStr | tr -cd "[0-9.]"`
-    versionStr=${versionStr:1}
-    frameworkPath="${newFilePath}/${podspec}-${versionStr}"
-    echo $frameworkPath 
-    #清空仓库
-    for file in ${gitPath}/*
-    do
-        if [[ $file =~ "${podspec}" ]];then
-            rm -rf $file
-            echo "删除" $podspec"/"$file
-        fi
-    done
-    # 拷贝到git 仓库
-    cp -r ${frameworkPath}/ios/ ${gitPath}/
-    cd ${gitPath} && git pull origin && git add . && git commit -m $versionStr && git push origin
+    # cd $newFilePath
+    # git init && git add . && git commit -m "build" 
+    # podspec=${sdkName/$oldPrefix/$newPrefix}
+    # pod package ${podspec}.podspec —force --spec-sources='https://github.com/CocoaPods/Specs.git,http://gerrit.3g.net.cn/gomo_ios_specs' --no-mangle --gomoad --exclude-deps
+    # versionStr=`sed -n "/s.version *=/p" ${podspec}.podspec`
+    # versionStr=`echo $versionStr | tr -cd "[0-9.]"`
+    # versionStr=${versionStr:1}
+    # frameworkPath="${newFilePath}/${podspec}-${versionStr}"
+    # echo $frameworkPath 
+    # #清空仓库
+    # for file in ${gitPath}/*
+    # do
+    #     if [[ $file =~ "${podspec}" ]];then
+    #         rm -rf $file
+    #         echo "删除" $podspec"/"$file
+    #     fi
+    # done
+    # # 拷贝到git 仓库
+    # cp -r ${frameworkPath}/ios/ ${gitPath}/
+    # cd ${gitPath} && git pull origin && git add . && git commit -m $versionStr && git push origin
 }
 main 
--- rubbishCode.sh
+++ rubbishCode.sh
@@ -1,127 +1,154 @@
 #! /bin/bash
 
-rubbishCodePath="/Users/zy/WorkSpace/Test/ShellTest/RubbishCode"
-
 classArray=('NSString' 'UILabel' 'NSDictionary' 'NSData' 'UIScrollView' 'UIView' 'NSObject' 'UIImage' 'NSArray' 'UIImageView' 'NSMutableString') #11
 methodNameArray=('action' 'setup' 'reset' 'pay' 'notificaiton' 'load' 'reload' 'status' 'progress' 'resume' 'cancel' 'add' 'remove') #13
 methodParamArray=('str' 'lab' 'dic' 'data' 'scrol' 'view' 'obj' 'img' 'arr' 'imgV' 'mutableStr') #11
 enArray=('a1' 'b1' 'c1' 'd1' 'e1' 'f1' 'g1' 'h1' 'i1' 'j1' 'k1' 'l1' 'm1' 'n1' 'o1' 'p1' 'q1' 'r1' 's1' 't1' 'u1' 'v1' 'w1' 's1' 'y1' 'z1') #26
 
+
 function judgeEnString(){
-	arr=${1}
-	newObjNameRand=`echo $RANDOM%26 | bc`
-	newObjName=${enArray[newObjNameRand]}
-	if [[ ${arr[*]} =~ newObjName && ${#arr[*]} > 1 ]]; then
-		judgeEnString $arr
+	rubbishEnArr=${1}
+	rubbishNewObjNameRand=`echo $RANDOM%26 | bc`
+	rubbishNewObjName=${enArray[rubbishNewObjNameRand]}
+	rubbishEnArrStr="${rubbishEnArr[*]}"
+	if [[ $rubbishEnArrStr =~ $rubbishNewObjName ]]; then
+		judgeEnString $rubbishEnArr
 	else
-		echo $newObjName
+		echo $rubbishNewObjName
 	fi
 }
 
 function judgeParmString(){
-	parmArr=${1}
-	parmNameRand=`echo $RANDOM%11 | bc`
-	pramClass=${classArray[parmNameRand]}
-	parmName=${methodParamArray[parmNameRand]}
-	if [[ ${parmArr[*]} =~ parmName && ${#parmArr[*]} > 1 ]]; then
-		judgeParmString $parmArr
+	rubbishParmArr=${1}
+	rubbishParmNameRand=`echo $RANDOM%11 | bc`
+	rubbishPramClass=${classArray[rubbishParmNameRand]}
+	rubbishParmName=${methodParamArray[rubbishParmNameRand]}
+	rubbishParamArrStr="${rubbishParmArr[*]}"
+	if [[ $rubbishParamArrStr =~ $rubbishParmName ]]; then
+		judgeParmString $rubbishParmArr
 	else
-		echo "(${pramClass} *)${parmName}"
+		echo "(${rubbishPramClass} *)${rubbishParmName}"
 	fi
 }
 
 function generateFuncContent(){
-	contentStr=''
 	# 生成方法内容
-	objArr=('')
+	rubbishObjArr=('')
 	#  随机初始化对象的个数
-	newObjRandNum=`echo $RANDOM%5+1 | bc`
-	for (( j = 0; j < $newObjRandNum; j++ )); do
+	rubbishNewObjRandNum=`echo $RANDOM%5+1 | bc`
+	for (( rubbishJ = 0; rubbishJ < $rubbishNewObjRandNum; rubbishJ++ )); do
 		# 判断name是否重复并获取一个不重复的
-		newObjName=`judgeEnString $objArr`
-		objArrIndex=$(echo ${#objArr[*]} | bc)
-		objArr[objArrIndex]=$newObjName
-		newClassRand=`echo $RANDOM%11 | bc`
-		objClass=${classArray[newClassRand]}
-		contentStr="${contentStr}\n ${objClass} *${newObjName} = [${objClass} new];"
+		rubbishewObjName=`judgeEnString $rubbishObjArr`
+		# objArrIndex=$(echo ${#rubbishObjArr[*]} | bc)
+		rubbishObjArr[${#rubbishObjArr[*]}]=$rubbishewObjName
+		rubbishNewClassRand=`echo $RANDOM%11 | bc`
+		rubbishObjClass=${classArray[rubbishNewClassRand]}
+		rubbishContentStr="${rubbishContentStr}\\ ${rubbishObjClass} *${rubbishewObjName} = [${rubbishObjClass} new];"
 	done
 	# 增加for循环
-	forinRandNum=`echo $RANDOM%5+1 | bc`
-	for (( i = 0; i < $forinRandNum; i++ )); do
-		forinRandCount=`echo $RANDOM%10+1 | bc`
-		forinStr="for (int i=0; i<${forinRandCount}; i++) {"
-		forinArr=('')
-		forinObjRandNum=`echo $RANDOM%5+1 | bc`
-		for (( m = 0; m < $forinObjRandNum; m++ )); do
+	rubbishForinRandNum=`echo $RANDOM%3+1 | bc`
+	for (( rubbishI = 0; rubbishI < $rubbishForinRandNum; rubbishI++ )); do
+		rubbishForinRandCount=`echo $RANDOM%50+1 | bc`
+		rubbishForinStr="for (int i=0; i<${rubbishForinRandCount}; i++) {"
+		rubbishForinArr=('')
+		rubbishForinObjRandNum=`echo $RANDOM%5+1 | bc`
+		for (( rubbishM = 0; rubbishM < $rubbishForinObjRandNum; rubbishM++ )); do
 			# 判断name是否重复并获取一个不重复的
-			forinObjName=`judgeEnString $forinArr`
-			forinArrIndex=$(echo ${#forinArr[*]} | bc)
-			forinArr[forinArrIndex]=$forinObjName
-			forinClassRand=`echo $RANDOM%11 | bc`
-			forinObjClass=${classArray[forinClassRand]}
-			forinStr="${forinStr}\n ${forinObjClass} *${forinObjName} = [${forinObjClass} new];"
+			rubbishForinObjName=`judgeEnString $rubbishForinArr`
+			# forinArrIndex=$(echo ${#rubbishForinArr[*]} | bc)
+			rubbishForinArr[${#rubbishForinArr[*]}]=$rubbishForinObjName
+			rubbishForinClassRand=`echo $RANDOM%11 | bc`
+			rubbishForinObjClass=${classArray[rubbishForinClassRand]}
+			rubbishForinStr="${rubbishForinStr}\\ ${rubbishForinObjClass} *${rubbishForinObjName} = [${rubbishForinObjClass} new];"
 		done
-		contentStr="${contentStr}\n${forinStr}\n}"
+		rubbishContentStr="${rubbishContentStr}\\${rubbishForinStr}\\}"
 	done
-	echo ${contentStr}
 }
 
 function generateFunc(){
 	# 随机取一个方法名
-	randMethodName=`echo $RANDOM%12 | bc`
-	methonName=${methodNameArray[randMethodName]}
+	rubbishRandMethodNum=`echo $RANDOM%13 | bc`
+	rubbishRandMethodName=${methodNameArray[rubbishRandMethodNum]}
 	# 判断方法名是否存在
-	if [ `grep -c ")$methonName" $file` -ne '0' ]; then
-		# echo found same method $methonName ,will reset 2
+	if [ `grep -c ")$rubbishRandMethodName" $rubbishFile` -ne '0' ]; then
+		echo found same method $rubbishRandMethodName ,will reset 
 		generateFunc
-		return
+	else
+		rubbishFuncStr="- (void)$rubbishRandMethodName"
+		# 随机传参个数
+		rubbishFandPramNum=`echo $RANDOM%2+1 | bc`
+		# 参数名数组
+		rubbishParamArray=('')
+		for (( rubbishZ = 0; rubbishZ<$rubbishFandPramNum; rubbishZ++ ))
+		do
+			# 随机取一个类
+			rubbishRandPramName=`judgeParmString $rubbishParamArray`
+			# rubbishParamArrayLastIndex=$(echo ${#rubbishParamArray[*]} | bc)
+			rubbishParamArray[${#rubbishParamArray[*]}]=$rubbishRandPramName
+			if [[ $i == 0 ]]; then
+				rubbishFuncStr="${rubbishFuncStr}With:${rubbishRandPramName} "
+			else
+				rubbishFuncStr="${rubbishFuncStr}with:${rubbishRandPramName} "
+			fi
+		done
+		rubbishFuncStr="${rubbishFuncStr}{"
+		# 获取内容
+		generateFuncContent 
+		rubbishInsetCode="${rubbishFuncStr}${rubbishContentStr}\\}\\"
+		# echo rubbishContentStr $rubbishContentStr
+		unset rubbishContentStr
 	fi
 
-	funcStr="- (void)$methonName"
-	# 随机传参个数
-	randPramNum=`echo $RANDOM%2+1 | bc`
-	# 参数名数组
-	paramArray=('')
-	for (( i = 0; i<$randPramNum; i++ ))
-	do
-		# 随机取一个类
-		randPramName=`judgeParmString $paramArray`
-		paramArrayLastIndex=$(echo ${#paramArray[*]}-1 | bc)
-		paramArray[paramArrayLastIndex]=$randPramName
-		if [[ $i == 0 ]]; then
-			funcStr="${funcStr}With:${randPramName} "
-		else
-			funcStr="${funcStr}with:${randPramName} "
-		fi
-	done
-	funcStr="${funcStr}{"
-	# 获取内容
-	contentString=`generateFuncContent`
-	funcStr="${funcStr}${contentString}\n}\n"
-	echo $funcStr
 }
 
 function insetFile(){
-	insetCode=${1?}
 
+	rubbishAllLineString=`grep '^[-+].*(.*).*' $rubbishFile`
+
+	rubbishLineArr=()
+	while [[ ${#rubbishAllLineString} != 0 ]]; do
+		# 条件需要修改 当{处于下一行时无法正常筛选
+		rubbishLineStr="${rubbishAllLineString%%{*}{"
+		rubbishLineStrlength=${#rubbishLineStr}
+		rubbishAllLineString=${rubbishAllLineString:${rubbishLineStrlength}}
+		rubbishLineArr[${#rubbishLineArr[*]}]=$rubbishLineStr
+		# echo rubbishLineStr $rubbishLineStr rubbishAllLineString $rubbishAllLineString
+	done
+
+	if [[ ${#rubbishLineArr[*]} > 0 ]]; then
+		rubbishInsetLineNum=`echo $RANDOM%${#rubbishLineArr[*]} | bc`
+		rubbishInsetLineStr=${rubbishLineArr[$rubbishInsetLineNum]}
+		rubbishInsetLineStr="${rubbishInsetLineStr#*)}"
+		rubbishInsetLineStr="${rubbishInsetLineStr%%:*}"
+		rubbishInsetLineStr="${rubbishInsetLineStr%%{*}"
+
+		if [[ ${#rubbishInsetLineStr} != 0 ]]; then
+			# 获取垃圾代码
+			generateFunc
+			#插入
+			sed -i '' -e "/)${rubbishInsetLineStr}/i\\
+			${rubbishInsetCode}" $rubbishFile
+			echo insert rubbishInsetLineStr $rubbishInsetLineStr rubbishInsetCode $rubbishInsetCode
+		fi
+		echo noinsert because no rubbishInsetLineStr rubbishFile $rubbishFile
+	else
+		echo noinsert because no rubbishLineArr rubbishFile $rubbishFile
+	fi
+	unset rubbishInsetCode
+	unset rubbishLineArr
 }
 
 function rubbishCode(){
-	file=${1?}
-	fileRegularStr='.m$'
-	if [[ ${file} =~ $fileRegularStr ]]; then
-		echo '在文件中生成垃圾代码 ' $file
+	rubbishFile=${1?}
+	rubbishfileRegularStr='.m$'
+	if [[ ${rubbishFile} =~ $rubbishfileRegularStr ]]; then
+		echo '在文件中生成垃圾代码 ' $rubbishFile
 		# 在.m中生成垃圾代码
-		 randNum=`echo $RANDOM%10+3 | bc`
-		 for (( r = 0; r < $randNum; r++ )); do
-			code=`generateFunc`
-			echo -e $code >> $file
+		 rubbishRandNum=`echo $RANDOM%4+1 | bc`
+		 echo rubbishRandNum $rubbishRandNum
+		 for (( rubbishR = 0; rubbishR < $rubbishRandNum; rubbishR++ )); do
+			insetFile
 		 done
 	fi
+	unset rubbishFile
 }
-
-for file in ${rubbishCodePath}/* ; do
-	if test -f $file ; then
-		rubbishCode $file
-	fi
-done
